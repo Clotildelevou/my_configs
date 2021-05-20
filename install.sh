6b32e79b73ff653bfe2a777b8565046c364a31a8
Clotilde Levesque <43673552+Clotildelevou@users.noreply.github.com>
2021-05-20 15:07:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt upgrade
 sudo add-apt-repository ppa:dawidd0811/neofetch
-sudo apt install tree vlc vim thunderbird neofetch i3 okular
+sudo apt install tree vlc vim thunderbird neofetch i3 okular firefox git
